870f7a1d35785e62f45409e0b0358b640f938a2b
asaloojee <asaloojee@omniwerx.io>
2026-04-18 01:26:35 -0400

    chore: progress on staff mgmt practice tool

--- s8-practice/src/main.rs
+++ s8-practice/src/main.rs
@@ -1,3 +1,38 @@
-fn main() {
-    println!("Hello, world!");
+use std::collections::HashMap;
+use std::io;
+
+fn main() -> io::Result<()> {
+    let departments = ["engineering", "sales", "finance", "legal", "culinary"];
+    let mut staff: HashMap<String, Vec<String>> = HashMap::new();
+
+    loop {
+        let mut buffer = String::new();
+        let stdin = io::stdin();
+        println!("Enter some text:");
+        stdin.read_line(&mut buffer)?;
+        let buffer = buffer.trim();
+
+        if buffer == "quit" {
+            break;
+        }
+
+        if buffer.starts_with("add") {
+            let words = buffer.split_whitespace().collect::<Vec<&str>>();
+            let subject = words[1];
+            let destination = words[3];
+
+            if departments.contains(&destination) {
+                staff
+                    .entry(String::from(destination))
+                    .or_default()
+                    .push(String::from(subject));
+            } else {
+                println!("Please select a valid department!");
+            }
+            println!("Staff: {:?}", staff);
+        }
+
+        println!("User said: {buffer}. Looping again");
+    }
+    Ok(())
 }
