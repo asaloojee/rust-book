22ea86f354345a33677549f6830a5f6eaf55c145
asaloojee <asaloojee@omniwerx.io>
2026-04-19 01:52:57 -0400

    chore: more progress, listing by dept and sorting works

--- s8-practice/src/main.rs
+++ s8-practice/src/main.rs
@@ -32,7 +32,31 @@ fn main() -> io::Result<()> {
             println!("Staff: {:?}", staff);
         }
 
-        println!("User said: {buffer}. Looping again");
+        if buffer.starts_with("list") {
+            let words = buffer.split_whitespace().collect::<Vec<&str>>();
+
+            if words.len() == 2 {
+                if words[1] == "all" {
+                    println!("All staff: \n {:?}", staff);
+                } else {
+                    println!("Please enter a valid list command!")
+                }
+            } else if words.len() == 3 {
+                let department = words[2];
+                if words[1] == "all" && departments.contains(&department) {
+                    if let Some(department_staff) = staff.get_mut(&String::from(department)) {
+                        department_staff.sort();
+                        println!("Staff in {department}: \n{:?}", department_staff);
+                    }
+                } else {
+                    println!("Please enter a valid list command!")
+                }
+            } else {
+                println!("Invalid list input. Try 'list all' or 'list all [department]'. ");
+            }
+        }
+
+        println!("======LOOP ENDED======");
     }
     Ok(())
 }
